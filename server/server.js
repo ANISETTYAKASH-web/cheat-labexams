468211064f23e84ef13fac582e47071ed15675d6
ANISETTYAKASH <akashanisetty7@gmail.com>
2025-07-01 09:15:28 +0100

    feat(server): intialized express server and saved files locally coming from server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer = require("multer");
+const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`listening on port:${PORT}`);
+});
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.array("files"), (req, res) => {
+  console.log(req.files);
+  res.json({ message: "success" });
+});
+app.get("/check", (req, res) => {
+  res.json({ message: "hey" });
+});
