c90b9b4a76c9d60fa5513f66e983ab52a7a4f276
ANISETTYAKASH <akashanisetty7@gmail.com>
2025-07-03 08:15:34 +0100

    Added url getting route to my server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const multer = require("multer");
+const S3URL = require("./services/S3Connect");
 const PORT = 3000;
 app.use(cors());
 app.use(express.json());
@@ -19,6 +20,7 @@ app.post("/upload", upload.array("files"), (req, res) => {
   console.log(req.files);
   res.json({ message: "success" });
 });
+app.use("/", S3URL);
 app.get("/check", (req, res) => {
   res.json({ message: "hey" });
 });
