2526bfb7bf1582fcd32a4c9dc3243bd18ba30883
ANISETTYAKASH <akashanisetty7@gmail.com>
2025-07-03 15:24:15 +0100

    feat(db config):used pool for database connections

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DB_STRING,
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
+});
+pool.on("connect", () => {
+  console.log("database connected");
+});
+pool.on("release", () => {
+  console.log("database released");
+});
+module.exports = pool;
