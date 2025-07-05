8deb83957fa75f7a48fbc8599b65bbf4c674200c
ANISETTYAKASH <akashanisetty7@gmail.com>
2025-07-05 11:23:57 +0100

    fix database error regarding deployment to render

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg");
+const url = require("url");
+const params = url.parse(process.env.DB_STRING);
+const auth = params.auth.split(":");
+console.log(auth[0]);
+console.log(auth[1]);
+console.log(params.hostname);
+console.log(params.port);
+console.log(params.pathname.split("/")[1]);
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  ssl: true,
+};
 
-const pool = new Pool({
-  connectionString: process.env.DB_STRING,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-});
-console.log(process.env.DB_STRING);
+const pool = new Pool(config);
+
 pool.on("connect", () => {
   console.log("database connected");
 });
